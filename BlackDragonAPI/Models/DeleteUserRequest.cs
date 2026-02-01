38be0767c5bfc1f6e5c83fa1c1898ed87db7fecc
terryreidhead <terryreidhead.tr@gmail.com>
2026-01-31 18:28:39 -0600

    User Profile work

---
+++ BlackDragonAPI/Models/DeleteUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BlackDragonAPI.Models
+{
+    public class DeleteUserRequest
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
--- BlackDragonAPI/Models/RegisterRequest.cs
+++ BlackDragonAPI/Models/RegisterRequest.cs
@@ -3,6 +3,7 @@
     public class RegisterRequest
     {
        public string Email { get; set; } = string.Empty;
-       public string Password { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
